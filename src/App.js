dba7a0c6348ed6aac20f65c55642f138bc782b5d
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-10-04 13:26:39 +0600

    assignment 9

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// import React, { createContext, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>this is home</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+
+  return (
+    <div className="NavBar__container">
+     
+    </div>
+  );
+};
+
+export default NavBar;
